9f3c093143c7330bc745fc0a85bf03c43f377717
Ground <27danny@naver.com>
2023-03-01 14:47:08 +0900

    New Porject

---
+++ wanted-ios-pre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted-ios-pre
+//
+//  Created by KimJS on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
